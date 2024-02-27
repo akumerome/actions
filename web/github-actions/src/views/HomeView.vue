bb7757d619fad770bda6bd2de542859d808b0852
Alvaro <akumenius@gmail.com>
2024-02-27 10:08:56 +0100

    calculator

--- web/github-actions/src/views/HomeView.vue
+++ web/github-actions/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="calculator">
+    <div v-for="n in 9" class="calculator_number"> {{ n }} </div>
+  </div>
 </template>
+
+<style>
+  .calculator {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  .calculator_number {
+    border: 1px solid black;
+    width: 75px;
+    height: 75px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
