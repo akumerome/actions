e8ce676686f4433e88e8589a50bf1e4963cbed57
Alvaro <akumenius@gmail.com>
2024-02-28 09:02:51 +0100

    commit

--- web/github-actions/src/views/HomeView.vue
+++ web/github-actions/src/views/HomeView.vue
@@ -1,24 +1,37 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    changeNumber(n) {
+      this.message = n;
+    }
+  }
+}
 </script>
 
 <template>
+  <p>El número seleccionat és {{ message }}</p>
   <div class="calculator">
-    <div v-for="n in 9" class="calculator_number"> {{ n }} </div>
+    <button v-for="n in 9" class="calculator_number" v-on:click="changeNumber(n)"> {{ n }} </button>
   </div>
 </template>
 
 <style>
-  .calculator {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-  }
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
 
-  .calculator_number {
-    border: 1px solid black;
-    width: 75px;
-    height: 75px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.calculator_number {
+  border: 1px solid black;
+  width: 75px;
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
