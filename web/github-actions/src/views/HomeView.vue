649835cb756cb66d973471207370d0b9c995a0d9
Alvaro <akumenius@gmail.com>
2024-02-28 10:51:47 +0100

    commit

--- web/github-actions/src/views/HomeView.vue
+++ web/github-actions/src/views/HomeView.vue
@@ -2,29 +2,35 @@
 export default {
   data() {
     return {
-      message: ''
-    }
+      message: '',
+    };
   },
   methods: {
     changeNumber(n) {
+      console.log(n);
       this.message = n;
-    }
-  }
-}
+    },
+  },
+};
 </script>
+
 
 <template>
   <p>El número seleccionat és {{ message }}</p>
   <div class="calculator">
-    <button v-for="n in 9" class="calculator_number" v-on:click="changeNumber(n)"> {{ n }} </button>
+    <button v-for="n in 9" :key="n" class="calculator_number" v-on:click="changeNumber(n)">
+      {{ n }}
+    </button>
   </div>
 </template>
+
 
 <style>
 .calculator {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
 }
+
 
 .calculator_number {
   border: 1px solid black;
